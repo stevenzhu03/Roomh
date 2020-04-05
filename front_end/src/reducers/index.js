6af918cde72f2268c4dbe43d1ce6ab999bb98a45
Steven Zhu <stevenzhu03@gmail.com>
2020-04-04 23:54:39 -0400

    implementing redux form

--- front_end/src/reducers/index.js
+++ front_end/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 import usersReducer from './usersReducer'
 import authReducer from './authReducer'
 
 export default combineReducers({
     users: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
