ad9e68e347e6bd474eadef9b35b4dc1303cdc8a7
Steven Zhu <stevenzhu03@gmail.com>
2020-04-13 22:55:47 -0400

    adding filter feature

--- front_end/src/components/Filter.js
+++ front_end/src/components/Filter.js
@@ -1,3 +1,52 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
 const Filter = (props) => {
-  return <div className="filter"></div>;
-};
+  const classes = useStyles();
+  const [gender, setGender] = React.useState('');
+
+
+  const handleChange = (event) => {
+    setGender(event.target.value)
+    props.handleGender(event.target.value)
+  };
+
+  return (
+    <div className="filter">
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-helper-label">Gender</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+          value={gender}
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={"male"}>Male</MenuItem>
+          <MenuItem value={"female"}>Female</MenuItem>
+        </Select>
+        <FormHelperText>Filter By Gender</FormHelperText>
+      </FormControl>
+    </div>
+  )
+
+}
+
+export default Filter
--- front_end/src/components/FindRoomate.js
+++ front_end/src/components/FindRoomate.js
@@ -1,23 +1,47 @@
 import React from "react";
 import UserCard from "./UserCard";
 import { fetchUsers } from "../actions";
+import Filter from "./Filter"
 import { connect } from "react-redux";
 
 class FindRoomate extends React.Component {
+
+  state = {
+    sex: "",
+    budget: null,
+    age: null,
+  }
+
+  handleGender = (value) => {
+    this.setState({sex: value})
+  }
 
   componentDidMount() {
     this.props.fetchUsers();
   }
 
+  filterUsers = (users) => {
+    if(this.state.sex){
+      const filterSex = users.filter(user => user.sex === this.state.sex)
+      return filterSex
+    }
+
+    return users
+  }
+
   render() {
-    
+    const filteredUsers = this.filterUsers(this.props.users)
+
     return (
       this.props.currentUser && (
-      <div className="ui four stackable cards">
-        {this.props.users.filter(user => user.id !== this.props.currentUser.info.id).map((user) => (
-          <UserCard key={user.id} {...user} />
-        ))}
-      </div>
+        <div className="find-roommate">
+          <Filter handleGender={this.handleGender}/>
+          <div className="ui four stackable cards">
+            {filteredUsers.filter(user => user.id !== this.props.currentUser.info.id).map((user) => (
+              <UserCard key={user.id} {...user} />
+            ))}
+          </div>
+        </div>
       )
     );
   }
